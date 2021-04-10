28a7be4910beb467d56d2fbd337818c6cae6edab
Kotwade <kotwade@yandex.ru>
2021-04-10 12:00:42 +0500

    Доработки

--- build/js/script.js
+++ build/js/script.js
@@ -1,11 +1,13 @@
 'use strict';
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.main-nav__toggle');
+var pageBody = document.querySelector('.page__body');
 
 pageHeader.classList.remove('page-header--nojs');
 
 if (headerToggle) {
   headerToggle.addEventListener('click', function () {
     pageHeader.classList.toggle('page-header--opened');
+    pageBody.classList.toggle('page__body--opened');
   });
 }
