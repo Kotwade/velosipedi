e7c04390521922c223e6c02b331b782e50bc6e0f
Kotwade <kotwade@yandex.ru>
2021-04-11 17:08:07 +0500

    Доработки

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.main-nav__toggle');
 var pageBody = document.querySelector('.page__body');
+var siteListLink = document.querySelectorAll('.site-list__link');
 
 pageHeader.classList.remove('page-header--nojs');
 
@@ -9,5 +10,11 @@ if (headerToggle) {
   headerToggle.addEventListener('click', function () {
     pageHeader.classList.toggle('page-header--opened');
     pageBody.classList.toggle('page__body--opened');
+  });
+
+  siteListLink.forEach(function (item) {
+    item.addEventListener('click', function () {
+      pageBody.classList.remove('page__body--opened');
+    });
   });
 }
