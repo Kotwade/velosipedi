df601537cc80bc3980cdc92511f9708667264f4e
Kotwade <kotwade@yandex.ru>
2021-04-16 12:18:33 +0500

    Исправление багов

--- build/js/script.js
+++ build/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.main-nav__toggle');
-var pageBody = document.querySelector('.page__body');
+var pageBody = document.querySelector('.page-body');
 var siteListLink = document.querySelectorAll('.site-list__link');
 
 pageHeader.classList.remove('page-header--nojs');
@@ -9,12 +9,12 @@ pageHeader.classList.remove('page-header--nojs');
 if (headerToggle) {
   headerToggle.addEventListener('click', function () {
     pageHeader.classList.toggle('page-header--opened');
-    pageBody.classList.toggle('page__body--opened');
+    pageBody.classList.toggle('page-body--opened');
   });
 
   siteListLink.forEach(function (item) {
     item.addEventListener('click', function () {
-      pageBody.classList.remove('page__body--opened');
+      pageBody.classList.remove('page-body--opened');
     });
   });
 }
